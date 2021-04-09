2847aebe2e65bd0f32623b1f010f38e7f15481a8
Patricia14 <patricia14103@gmail.com>
2021-04-08 22:03:15 -0600

    Creacion splash screen
    splash screen y mensajes toast

--- app/src/main/java/com/example/desafio1ejer3/MainActivity.java
+++ app/src/main/java/com/example/desafio1ejer3/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.desafio1ejer3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimerTask tarea =new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,PrimeraActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,3000);
     }
 
-    public void Ingresar(View in){
-        Intent calculos = new Intent(this, MedicineActivity.class);
-        startActivity(calculos);
-    }
+
+
+
 }
