bc2685d5baf78d96361609dcb054eb4f8fe399ab
cmar01 <reyalexmendoza@gmail.com>
2021-04-09 13:00:18 -0600

    Revert "Merge branch 'main' of https://github.com/Patricia14/Parcial2_DSM into main"
    This reverts commit dccabf8344e6d6c63ae8de10ea5b9a60a961c3f2, reversing
    changes made to 1e5ee9182af31977e821f9dee3312c2ffd6ecad2.

--- app/src/main/java/com/example/desafio1ejer3/FacturaActivity.java
+++ app/src/main/java/com/example/desafio1ejer3/FacturaActivity.java
@@ -3,16 +3,13 @@ package com.example.desafio1ejer3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FacturaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_factura);
-        Toast.makeText(getApplicationContext(), "Compra realizada", Toast.LENGTH_SHORT).show();
-    }
+        setContentView(R.layout.activity_factura);}
 
     public void Atras(View in){
         Intent antes = new Intent(this, MainActivity.class);
